1f259e3f6a6b769985d1af3b80ac1c36b9d551a8
emanuelvsz <usermane13@gmail.com>
2023-08-08 00:54:54 -0300

    commitando da develop

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	e.GET("/", hello)
+
+	e.Start(":8080")
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Olá, mundo!")
+}
