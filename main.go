4c621b6fa6e68d9a53b6a35a6c1c3105541bd611
emanuelvsz <usermane13@gmail.com>
2023-08-08 01:15:56 -0300

    creatin models and slice

--- main.go
+++ main.go
@@ -1,13 +1,55 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
+type animal struct {
+	id            int
+	name          string
+	cientificName string
+	pictureURL    string
+}
+
+var animals = []animal{
+	{
+		id: 1,
+		name: "",
+		cientificName: "",
+		pictureURL: "",
+	},
+	{
+		id: 2,
+		name: "",
+		cientificName: "",
+		pictureURL: "",
+	},
+	{
+		id: 3,
+		name: "",
+		cientificName: "",
+		pictureURL: "",
+	},
+	{
+		id: 4,
+		name: "",
+		cientificName: "",
+		pictureURL: "",
+	},
+}
+
 func main() {
 	e := echo.New()
+
+	jsonData, err := json.Marshal(animals)
+	if err != nil {
+		fmt.Println("Erro ao converter para JSON:", err)
+		return
+	}
 
 	e.GET("/", hello)
 
